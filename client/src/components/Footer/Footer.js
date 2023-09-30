5de863021505c8473e36f67e2546b5f80728e62c
PrimoKu <primoku.dev@gmail.com>
2023-09-30 14:29:31 -0400

    230930 - Add Components, Layouts, and Views

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+// reactstrap components
+import { Container, Nav, NavItem, NavLink } from "reactstrap";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Container fluid>
+        <Nav>
+          <NavItem>
+            <NavLink href="">
+              BlueJay
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="">
+              About Us
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <div className="copyright">
+          © {new Date().getFullYear()} made with{" "}
+          <i className="tim-icons icon-heart-2" /> by{" "}
+          <a
+            href="https://www.creative-tim.com/?ref=bdr-user-archive-footer"
+            target="_blank"
+          >
+            Creative Tim
+          </a>{" "}
+          for a better web.
+        </div>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
