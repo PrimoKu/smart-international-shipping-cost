474201e27f759fe56501d23d3e3c06e4c0a073ec
ZixuanLiu-Justin <zliu189@jhu.edu>
2023-10-16 16:53:56 -0400

    add pages

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect } from 'react';
 import axios from 'axios';
+import logo from '../assets/img/Logo.png';
+import earth from '../assets/img/earth.png';
+import plane from '../assets/img/plane.jpg';
+import '../assets/css/image.css';
 import {
     Button,
     Card,
@@ -14,10 +20,13 @@ import {
     Col,
 } from "reactstrap";
 
+
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loginError, setLoginError] = useState("");
+    const [imageSize] = useState(60);
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -38,17 +47,55 @@ function Login() {
         });
     };
 
+    const controls = useAnimation();
+
+    useEffect(() => {
+        const sequence = async () => {
+            await controls.start({ pathOffset: 0 });
+            await controls.start({ pathOffset: 1, rotate: 180 });
+            await controls.start({ pathOffset: 0, rotate: 0 });
+        };
+        sequence();
+    }, [controls]);
+
     return (
         <div className="content" style={{ overflowX: 'hidden' }}>
             <Row className="justify-content-center">
                 <Col xl="10" lg="12" md="9">
                     <Card className="o-hidden border-0 shadow-lg my-5 py-5">
                         <CardHeader>
-                            <h1 className="text-center title mt-5">Login</h1>
+                            <h1 className="text-center title mt-5">Welcome Back!</h1>
                         </CardHeader>
                         <CardBody>
                             <Row>
-                                <Col lg>
+                                <Col lg="6" className="d-flex justify-content-center align-items-center">
+                                    <div className="image-container">
+                                        <img src={logo} style={{ width: `${imageSize}%` }} alt="Logo" />
+                                    </div>
+                                </Col>
+                                <Col lg="6">
+                                    <h2 className="text-center title mt-5">Login</h2>
+                                    <svg width="1500" height="100">
+                                        <image xlinkHref={earth} x="50" y="55" width="20" height="20"/>
+                                        <image xlinkHref={earth} x="950" y="55" width="20" height="20"/>
+                                        
+                                        <motion.image 
+                                            xlinkHref={plane} 
+                                            x="10" 
+                                            y="50" 
+                                            width="30" 
+                                            height="30"
+                                            animate={{
+                                                x: [150, 920, 5]
+                                            }}
+                                            transition={{
+                                                duration: 15,
+                                                repeat: Infinity,
+                                                ease: "linear"
+                                            }}
+                                        />
+                                    </svg>
+                                    
                                     <div className="px-5">
                                         <Form onSubmit={handleSubmit} id="form_user">
                                             <FormGroup>
--- client/src/views/UserProfile.js
+++ client/src/views/UserProfile.js
@@ -1,203 +1,230 @@
-/*!
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-=========================================================
-* Black Dashboard React v1.2.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
-
-// reactstrap components
 import {
   Button,
   Card,
   CardHeader,
   CardBody,
   CardFooter,
-  CardText,
+  CardText,  // <-- Make sure this line is here
   FormGroup,
   Form,
   Input,
+  Label,
   Row,
   Col,
 } from "reactstrap";
 
 function UserProfile() {
+  const [showUserEdit, setShowUserEdit] = useState(false);
+  const [showPaymentEdit, setShowPaymentEdit] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/users/login', { withCredentials: true });
+        console.log(response);
+        setData(response.data);
+      } catch (error) {
+        console.error("An error occurred while fetching data", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="content">
         <Row>
           <Col md="8">
+            {/* User Information Card */}
             <Card>
               <CardHeader>
-                <h5 className="title">Edit Profile</h5>
+                <h2 className="title">User Information</h2>
               </CardHeader>
               <CardBody>
+                {!showUserEdit ? (
+                  <div>
+                  <p>User ID: {data._id || 'Loading...'}</p>
+                  <p>User Name: {data.name || 'Loading...'}</p>
+                  <p>Email: {data.email || 'Loading...'}</p>
+                  <p>Password: [Your Password]</p>
+                  <p>Legal Name: [Your Legal Name]</p>
+                  <p>Legal ID: [Your Legal ID]</p>
+                  <p>Gender: [Your Gender]</p>
+                  <p>Birth Date: [Your Birth Date]</p>
+                </div>
+                ) : (
                 <Form>
-                  <Row>
-                    <Col className="pr-md-1" md="5">
-                      <FormGroup>
-                        <label>Company (disabled)</label>
-                        <Input
-                          defaultValue="Creative Code Inc."
-                          disabled
-                          placeholder="Company"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-md-1" md="3">
-                      <FormGroup>
-                        <label>Username</label>
-                        <Input
-                          defaultValue="michael23"
-                          placeholder="Username"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-md-1" md="4">
-                      <FormGroup>
-                        <label htmlFor="exampleInputEmail1">
-                          Email address
-                        </label>
-                        <Input placeholder="mike@email.com" type="email" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-md-1" md="6">
-                      <FormGroup>
-                        <label>First Name</label>
-                        <Input
-                          defaultValue="Mike"
-                          placeholder="Company"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-md-1" md="6">
-                      <FormGroup>
-                        <label>Last Name</label>
-                        <Input
-                          defaultValue="Andrew"
-                          placeholder="Last Name"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="12">
-                      <FormGroup>
-                        <label>Address</label>
-                        <Input
-                          defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                          placeholder="Home Address"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-md-1" md="4">
-                      <FormGroup>
-                        <label>City</label>
-                        <Input
-                          defaultValue="Mike"
-                          placeholder="City"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="px-md-1" md="4">
-                      <FormGroup>
-                        <label>Country</label>
-                        <Input
-                          defaultValue="Andrew"
-                          placeholder="Country"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-md-1" md="4">
-                      <FormGroup>
-                        <label>Postal Code</label>
-                        <Input placeholder="ZIP Code" type="number" />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="8">
-                      <FormGroup>
-                        <label>About Me</label>
-                        <Input
-                          cols="80"
-                          defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in
-                            that two seat Lambo."
-                          placeholder="Here can be your description"
-                          rows="4"
-                          type="textarea"
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
+                    <Row>
+                        {/* User ID & User Name */}
+                        <Col className="pr-md-1" md="6">
+                            <FormGroup>
+                                <Label>User ID</Label>
+                                <Input placeholder="User ID" type="text" />
+                            </FormGroup>
+                        </Col>
+                        <Col className="pl-md-1" md="6">
+                            <FormGroup>
+                                <Label>User Name</Label>
+                                <Input placeholder="User Name" type="text" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        {/* Email & Password */}
+                        <Col className="pr-md-1" md="6">
+                            <FormGroup>
+                                <Label>Email</Label>
+                                <Input placeholder="Email" type="email" />
+                            </FormGroup>
+                        </Col>
+                        <Col className="pl-md-1" md="6">
+                            <FormGroup>
+                                <Label>Password</Label>
+                                <Input placeholder="Password" type="password" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        {/* Legal Name & Legal ID */}
+                        <Col className="pr-md-1" md="6">
+                            <FormGroup>
+                                <Label>Legal Name</Label>
+                                <Input placeholder="Legal Name" type="text" />
+                            </FormGroup>
+                        </Col>
+                        <Col className="pl-md-1" md="6">
+                            <FormGroup>
+                                <Label>Legal ID</Label>
+                                <Input placeholder="Legal ID" type="text" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        {/* Gender & Birth Date */}
+                        <Col className="pr-md-1" md="6">
+                            <FormGroup>
+                                <Label>Gender</Label>
+                                <Input type="select">
+                                    <option>Male</option>
+                                    <option>Female</option>
+                                    <option>Other</option>
+                                </Input>
+                            </FormGroup>
+                        </Col>
+                        <Col className="pl-md-1" md="6">
+                            <FormGroup>
+                                <Label>Birth Date</Label>
+                                <Input type="date" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
                 </Form>
+                )}
               </CardBody>
               <CardFooter>
-                <Button className="btn-fill" color="primary" type="submit">
-                  Save
+                <Button onClick={() => setShowUserEdit(!showUserEdit)}>
+                  {showUserEdit ? "Cancel" : "Edit"}
                 </Button>
+                {showUserEdit && <Button onClick={() => setShowUserEdit(false)}>Confirm</Button>}
               </CardFooter>
             </Card>
-          </Col>
-          <Col md="4">
-            <Card className="card-user">
+
+            {/* Payment Information Card */}
+            <Card>
+              <CardHeader>
+                <h2 className="title">Payment Information</h2>
+              </CardHeader>
               <CardBody>
-                <CardText />
-                <div className="author">
-                  <div className="block block-one" />
-                  <div className="block block-two" />
-                  <div className="block block-three" />
-                  <div className="block block-four" />
-                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                    <img
-                      alt="..."
-                      className="avatar"
-                      src={require("assets/img/emilyz.jpg")}
-                    />
-                    <h5 className="title">Mike Andrew</h5>
-                  </a>
-                  <p className="description">Ceo/Co-Founder</p>
+                {!showPaymentEdit ? (
+                  <div>
+                  <p>Credit Card Type: [Your Credit Card Type]</p>
+                  <p>Bank Name: [Your Bank Name]</p>
+                  <p>Bank Account Number: [Your Bank Account Number]</p>
+                  <p>Payment: [Your Payment]</p>
+                  <p>Address 1: [Your Address 1]</p>
+                  <p>Address 2: [Your Address 2]</p>
+                  <p>State: [Your State]</p>
+                  <p>City: [Your City]</p>
+                  <p>Zip Code: [Your Zip Code]</p>
                 </div>
-                <div className="card-description">
-                  Do not be scared of the truth because we need to restart the
-                  human foundation in truth And I love you like Kanye loves
-                  Kanye I love Rick Owens’ bed design but the back is...
-                </div>
+                ) : (
+                  <Form>
+                    <Row>
+                      <Col className="pr-md-1" md="5">
+                        <FormGroup>
+                          <Label>Credit Card Type</Label>
+                          <Input placeholder="Credit Card Type" type="text" />
+                        </FormGroup>
+                      </Col>
+                      <Col className="px-md-1" md="3">
+                        <FormGroup>
+                          <Label>Bank Name</Label>
+                          <Input placeholder="Bank Name" type="text" />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-md-1" md="4">
+                        <FormGroup>
+                          <Label>Bank Account Number</Label>
+                          <Input placeholder="Bank Account Number" type="text" />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-md-1" md="5">
+                        <FormGroup>
+                          <Label>Payment</Label>
+                          <Input placeholder="Payment" type="text" />
+                        </FormGroup>
+                      </Col>
+                      <Col className="px-md-1" md="7">
+                        <FormGroup>
+                          <Label>Address 1</Label>
+                          <Input placeholder="Address 1" type="text" />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-md-1" md="5">
+                        <FormGroup>
+                          <Label>Address 2</Label>
+                          <Input placeholder="Address 2 (optional)" type="text" />
+                        </FormGroup>
+                      </Col>
+                      <Col className="px-md-1" md="3">
+                        <FormGroup>
+                          <Label>State</Label>
+                          <Input placeholder="State" type="text" />
+                        </FormGroup>
+                      </Col>
+                      <Col className="pl-md-1" md="4">
+                        <FormGroup>
+                          <Label>City</Label>
+                          <Input placeholder="City" type="text" />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-md-1" md="5">
+                        <FormGroup>
+                          <Label>Zip Code</Label>
+                          <Input placeholder="Zip Code" type="text" />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                  </Form>
+                )}
               </CardBody>
               <CardFooter>
-                <div className="button-container">
-                  <Button className="btn-icon btn-round" color="facebook">
-                    <i className="fab fa-facebook" />
-                  </Button>
-                  <Button className="btn-icon btn-round" color="twitter">
-                    <i className="fab fa-twitter" />
-                  </Button>
-                  <Button className="btn-icon btn-round" color="google">
-                    <i className="fab fa-google-plus" />
-                  </Button>
-                </div>
+                <Button onClick={() => setShowPaymentEdit(!showPaymentEdit)}>
+                  {showPaymentEdit ? "Cancel" : "Edit"}
+                </Button>
+                {showPaymentEdit && <Button onClick={() => setShowPaymentEdit(false)}>Confirm</Button>}
               </CardFooter>
             </Card>
           </Col>
