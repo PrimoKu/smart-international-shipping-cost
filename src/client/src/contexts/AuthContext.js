d8a9be2b62e6bbf37b820ff3ed835061157f8f51
PrimoKu <primoku.dev@gmail.com>
2023-11-01 13:22:03 -0400

    231101 - Client Update 1. Implement socket to get notifications in real-time 2. Create invite joiners UI to 'GroupOrder' 3. Add new dependencies

--- src/client/src/contexts/AuthContext.js
+++ src/client/src/contexts/AuthContext.js
@@ -1,37 +1,138 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import { io } from 'socket.io-client';
+import {
+    Modal,
+    ModalHeader,
+    UncontrolledAlert,
+    Alert
+} from "reactstrap";
+import { Toast } from 'primereact/toast';
+import { Button } from 'primereact/button';
+import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [socket, setSocket] = useState(null);
     const navigate = useNavigate();
+    const [parsedMessage, setParsedMessage] = useState({ text: '', link: '' });
+    const [modalNotification, setModalNotification] = useState(false);
+    const [groupOrderId, setGroupOrderId] = useState('');
+    const toastRef = useRef(null);
+
+    const showNotification = (parsedMessage) => {
+        const messageContent = (
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <div className="text-dark" style={{ fontSize: '20px' }}>
+                    {parsedMessage.text}
+                    {parsedMessage.link && (
+                        <a href={parsedMessage.link} target="_blank" rel="noopener noreferrer"> Click here</a>
+                    )}
+                </div>
+                {parsedMessage.link && (
+                    <div style={{ marginTop: '15px', display: 'flex', gap: '10px' }}>
+                        <Button label="Accept" className="p-button-success" onClick={acceptInvite}/>
+                        <Button label="Decline" className="p-button-danger" onClick={() => toastRef.current.clear()} />
+                    </div>
+                )}
+            </div>
+        );
+        toastRef.current.show({ 
+            severity: 'info', summary: 'Notification', detail: messageContent, life: 10000 
+        });
+    };
+
+    const acceptInvite = async () => {
+        console.log(groupOrderId);
+        axios.put(`http://localhost:8080/api/groupOrders/add/${groupOrderId}`, { withCredentials: true })
+        .then(response => {
+            window.location.href = parsedMessage.link;
+        })
+        .catch((error) => {
+            if (error.response && error.response.data) {
+            console.log(error.response);
+            }
+        });
+    }
 
     React.useEffect(() => {
         const fetchUser = async () => {
             try {
                 const res = await axios.get('http://localhost:8080/api/users/current', { withCredentials: true });
                 if (res.data) {
-                    console.log(res.data);
+                    // console.log(res.data);
                     setUser(res.data);
-                    setIsAuthenticated(res.data !== null);
+                    setIsAuthenticated(true);
+                    if(!socket) {
+                        const socketInstance = io('http://localhost:8080');
+                        setSocket(socketInstance);
+
+                        socketInstance.on('connect', () => {
+                            console.log('Connected to server');
+                            socketInstance.emit('register', res.data._id);
+                        });
+
+                        socketInstance.on('notification', (message) => {
+                            console.log(message);
+                            const match = message.match(/(http[^\s]+)/);
+                            let newParsedMessage;
+                            if (match) {
+                                newParsedMessage = {
+                                    text: message.replace(match[0], '').trim(),
+                                    link: match[0]
+                                };
+                                var segments = match[0].split('/');
+                                var lastSegment = segments.pop();
+                                console.log(lastSegment);
+
+                                setGroupOrderId(lastSegment);
+                            } else {
+                                newParsedMessage = { text: message, link: '' };
+                            }
+                        
+                            showNotification(newParsedMessage);
+                        
+                            setParsedMessage(newParsedMessage);
+                        });
+                    }
                 } else {
-                    // console.log("No User");
-                    setUser(null);
-                    setIsAuthenticated(res.data === null);
-                    navigate('/');
+                    handleLogout();
                 }
             } catch (error) {
-                navigate('/');
+                handleLogout();
             }
         }
-        fetchUser();
-    }, []);
 
+        const handleLogout = () => {
+            if(socket) {
+                socket.disconnect();
+                setSocket(null);
+            }
+            setUser(null);
+            setIsAuthenticated(false);
+            navigate('/');
+        }
+
+        fetchUser();
+
+        return () => {
+            if(socket) {
+                socket.disconnect();
+            }
+        }
+    }, [navigate, socket]);
+
+    const toggleNotification = () => {
+        setModalNotification(!modalNotification);
+    };
+    
     return (
         <AuthContext.Provider value={{ user, setUser, isAuthenticated, setIsAuthenticated }}>
+            <Toast ref={toastRef} position="top-center" style={{ backgroundColor: 'white' }}/>
             { children }
         </AuthContext.Provider>
     );
