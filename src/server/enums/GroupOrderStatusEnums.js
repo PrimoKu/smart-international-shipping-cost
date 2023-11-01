cb92f87f2e88485ea8a9366a6409326f9acd298d
PrimoKu <primoku.dev@gmail.com>
2023-11-01 19:03:33 -0400

    231101 - Server Update 1. Change GroupStatusEnums

--- src/server/enums/GroupOrderStatusEnums.js
+++ src/server/enums/GroupOrderStatusEnums.js
@@ -1,14 +1,14 @@
 const GroupOrderStatus = {
-    CANCELED: 0,
-    PENDING: 1,
+    PENDING: 0,
+    CANCELED: 1,
     SUBMITTED: 2,
     SHIPPING: 3,
     DELIVERED: 4
 };
 
 const GroupOrderStatusList = [
-    { text: 'Canceled', name:'canceled', value: OrderStatus.CANCELED },
     { text: 'Pending', name:'pending', value: OrderStatus.PENDING },
+    { text: 'Canceled', name:'canceled', value: OrderStatus.CANCELED },
     { text: 'Submitted', name:'submitted', value: OrderStatus.SUBMITTED },
     { text: 'Shipping', name:'shipping', value: OrderStatus.SHIPPING },
     { text: 'Delivered', name:'delivered', value: OrderStatus.DELIVERED },
