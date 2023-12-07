88a8d168fdc41a5febfc8a288fddea99935f654d
刘子轩 <liuzixuan@liuzixuandeMacBook-Pro.local>
2023-12-06 19:53:18 -0500

    add coupon

--- src/client/src/views/Coupon.js
+++ src/client/src/views/Coupon.js
@@ -1,76 +1,101 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import { useAuth } from "contexts/AuthContext.js";
+import { Button, Card, CardHeader, CardBody, FormGroup, Form, Input, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label } from "reactstrap";
 
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  CardText,  // <-- Make sure this line is here
-  FormGroup,
-  Form,
-  Input,
-  Label,
-  Row,
-  Col,
-} from "reactstrap";
-
-function UserProfile() {
+function Coupon() {
   const { user } = useAuth();
 
-  const [shipmentDetails, setShipmentDetails] = useState({
-    // _id: '',
-    firstName: user.shipment.first_name,
-    lastName: user.shipment.last_name,
-    address1: user.shipment.address_1,
-    address2: user.shipment.address_2,
-    state: user.shipment.state,
-    city: user.shipment.city,
-    zipCode: user.shipment.zip_code,
-  });
+  const [coupons, setCoupons] = useState([]);
+  const [couponCode, setCouponCode] = useState("");
+  const [modal, setModal] = useState(false); // State to control modal visibility
 
-  console.log(user)
-
-  const updateShipment = async () => {
-    let formData = new FormData();
-    // formData.append('id', user.shipment._id);
-    formData.append('firstName', shipmentDetails.firstName);
-    formData.append('lastName', shipmentDetails.lastName);
-    formData.append('address1', shipmentDetails.address1);
-    formData.append('address2', shipmentDetails.address2);
-    formData.append('state', shipmentDetails.state);
-    formData.append('city', shipmentDetails.city);
-    formData.append('zipCode', shipmentDetails.zipCode);
-
-    axios.post('http://localhost:8080/api/shipments/upsert', formData, { withCredentials: true })
+  useEffect(() => {
+    axios.get('/userCoupons')
       .then(response => {
-          window.location.reload();
-      })
-      .catch((error) => {
-        if (error.response && error.response.data) {
-          console.log(error.response);
+        if (response.status === 200) {
+          setCoupons(response.data.coupons);
         }
-      });
-  }
+      })
+      .catch(error => console.error("Error fetching coupons:", error));
+  }, []);
+
+  const toggleModal = () => setModal(!modal); // Function to toggle modal visibility
+
+  const handleAddCoupon = (e) => {
+    e.preventDefault();
+    axios.post('/userCoupons', { coupon_code: couponCode })
+      .then(response => {
+        if (response.status === 200) {
+          setCoupons([...coupons, response.data]);
+          setCouponCode("");
+          toggleModal(); // Close the modal after adding the coupon
+        }
+      })
+      .catch(error => console.error("Error adding coupon:", error));
+  };
 
   return (
     <>
       <div className="content">
         <Row>
           <Col md="12">
-            {/* User Information Card */}
             <Card>
               <CardHeader>
-                <h2 className="title">Coupon System</h2>
+                <h2 className="title">Your Coupons</h2>
+                <Button color="primary" onClick={toggleModal}>Add Coupon</Button> {/* Button to open modal */}
               </CardHeader>
+              <CardBody>
+                {/* Coupon Display */}
+                {coupons.length > 0 ? (
+                  coupons.map(coupon => (
+                    <div key={coupon._id}>
+                      <p>Name: {coupon.name}</p>
+                      <p>Code: {coupon.code}</p>
+                      <p>Discount: {coupon.discount}</p>
+                      <p>Expires on: {coupon.expire_date}</p>
+                    </div>
+                  ))
+                ) : (
+                  <p>No coupons available.</p>
+                )}
+              </CardBody>
             </Card>
           </Col>
         </Row>
       </div>
+
+      {/* Modal for Adding Coupon */}
+      <Modal isOpen={modal} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Add a Coupon</ModalHeader>
+        <Form onSubmit={handleAddCoupon}>
+          <ModalBody>
+            <FormGroup>
+              <Label for="couponCode">Coupon Code</Label>
+              <Input
+                type="text"
+                name="couponCode"
+                id="couponCode"
+                placeholder="Enter coupon code"
+                style={{ height: '50px', fontSize: '18px', color: 'black' }}
+                value={couponCode}
+                onChange={(e) => setCouponCode(e.target.value)}
+              />
+            </FormGroup>
+          </ModalBody>
+{/* 
+          <ModalFooter style={{display: 'flex', justifyContent: 'flex-end', padding: '1rem'}}>
+                  <Button color="secondary" onClick={toggleCreateGroupOrderModal} className="btn-secondary mx-1" style={createGroupOrderModalCancelable ? {} : { display: 'none' }}>Close</Button>
+              </ModalFooter> */}
+
+          <ModalFooter style={{display: 'flex', justifyContent: 'flex-end', padding: '1rem'}}>
+            <Button color="primary" type="submit" className="btn-secondary mx-1">Add Coupon</Button>{' '}
+            <Button color="secondary" onClick={toggleModal} className="btn-secondary mx-1">Cancel</Button>
+          </ModalFooter>
+        </Form>
+      </Modal>
     </>
   );
 }
 
-export default UserProfile;
+export default Coupon;
